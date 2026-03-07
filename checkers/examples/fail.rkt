a8b0522176e2aa9461b09974e25dd2c8b950eccb
Ryan Culpepper <ryanc@racket-lang.org>
2026-03-07 11:58:40 -0500

    add examples

---
+++ checkers/examples/fail.rkt
@@ -0,0 +1,64 @@
+;; Copyright 2026 Ryan Culpepper
+;; SPDX-License-Identifier: Apache-2.0
+
+#lang racket/base
+(require racket/match
+         racket/list
+         raco/testing
+         checkers)
+
+(test #:name "outer"
+  (test #:name "inner"
+    (check 1 #:is 2))
+  (check 1 #:is 3))
+
+(test (check 1 #:is 2))
+(test (check 1 #:is (values 1 2)))
+(test (check (values 1 2) #:is 0))
+(test (check (values 1 2) #:is (values 2 3)))
+(test (check (/ 1 0) #:is 0))
+(test (check (/ 1 0) #:is (values 1 2)))
+
+(test (check 1 #:is-not 1))
+(test (check 1 #:is-not (values 1 2)))
+(test (check (values 1 2) #:is-not 0))
+(test (check (values 1 2) #:is-not (values 1 2)))
+(test (check (/ 1 0) #:is-not 0))
+(test (check (/ 1 0) #:is-not (values 1 2)))
+
+(test (check #f #:is-true))
+(test (check (values 1 2) #:is-true))
+(test (check (/ 1 0) #:is-true))
+
+(test (check 1 #:match (list x y)))
+(test (check (values 1 2) #:match (list x y)))
+(test (check (/ 1 0) #:match (list x y)))
+
+(test (check 1 #:error exn:fail?))
+(test (check 1 #:error #rx"wrong"))
+(test (check (values 1 2) #:error exn:fail?))
+(test (check (values 1 2) #:error #rx"wrong"))
+(test (check (error 'foo) #:error exn:fail:contract?))
+(test (check (error 'foo) #:error #rx"bar: ^"))
+
+(test (check (error 'foo) #:no-error))
+
+(test (check 1 #:with even?))
+(test (check (values 1 2) #:with even?))
+(test (check (/ 1 0) #:with even?))
+
+(test (check 1 #:with (checker:predicate void #b100)))
+(test (check (values 1 2) #:with (checker:predicate void #b111000)))
+(test (check (values 1 2) #:with (checker:predicate void (arithmetic-shift -1 3))))
+(test (check (/ 1 0) #:with (checker:predicate void)))
+
+(test (check 1 #:with (checker:compare > 10)))
+(test (check (values 1 2) #:with (checker:compare > 10)))
+(test (check (/ 1 0) #:with (checker:compare > 10)))
+
+
+(match (test-report)
+  [(cons failed total)
+   (unless (= failed total)
+     (error 'examples "not all tests failed!\n  total: ~s\n  failed: ~s"
+            total failed))])
---
+++ checkers/examples/pass.rkt
@@ -0,0 +1,42 @@
+;; Copyright 2026 Ryan Culpepper
+;; SPDX-License-Identifier: Apache-2.0
+
+#lang racket/base
+(require racket/match
+         racket/list
+         checkers)
+
+(test #:name "identity"
+  (check (+ 7 0) #:is 7)
+  (check (* 8 1) #:is 8))
+
+(test #:name "arithmetic"
+  (check (+ 1 2) #:is 3)
+  (check (+ 4 6) #:with even?)
+  (check (quotient/remainder 10 3) #:is (values 3 1))
+  (check (/ 1 0) #:error exn:fail:contract:divide-by-zero?))
+
+(test
+  (check (modulo 5 0)
+    #:error exn:fail:contract:divide-by-zero?
+    #:error #rx"^modulo: "))
+(test
+  (check (range 10)
+    #:with list?
+    #:with (lambda (v) (= (length v) 10))))
+(test
+  (check (range 10)
+    #:match (list (? exact-nonnegative-integer?) ...)))
+
+(test (check 1 #:is-true))
+(test (check 1 #:is-not 2))
+(test (check 1 #:no-error))
+
+;; check does not catch exn:break
+(test (check (with-handlers ([exn:break? (lambda (e) 'pass)])
+               (check (break-thread (current-thread)) #:error exn:break?)
+               'fail)
+        #:is 'pass))
+
+;; check catches exn:fail (subtype), even w/o check clauses
+(test (check (/ 1 0)))
